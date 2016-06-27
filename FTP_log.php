6d4e18f3069e72adf0d7022dacb9feca6a5f0a50
ModernMantra <kerimnjuhovic@gmail.com>
2016-06-28 00:25:18 +0200

    Add files via upload

---
+++ FTP_log.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+h1{
+	font-family: "Helvetica";
+	color: Gray;
+	font-weight: 200;
+	margin: 0 auto;
+	width: 300px;
+	margin-top: 50px;
+}
+#contentBody{
+	margin-top: 20px;
+	font-family: "Helvetica";
+	font-weight: 100;
+	font-size: 11px;
+	color: grey;
+	margin: 0 auto;
+	width: 750px;
+}
+.lines{
+	width: 800px;
+	margin: 0 auto;
+}
+footer{
+	font-family: "HelveticaNeue-UltraLight";
+	width: 200px;
+	margin: 0 auto;
+}
+</style>
+		<head>
+			<title>File Transfer Log Details</title>
+		</head>
+		<body>
+			<h1> File Transfer Details</h1>
+			<br>
+			<div class="lines"><hr noshade size="1px"></div>
+			<div id="contentBody">
+				<?php include("fileNo77.html"); ?> 
+			</div>
+			<div class="lines"><hr noshade size="1px"></div>
+			<footer>Computer Networks CS408</footer>
+		</body>
+</html>
